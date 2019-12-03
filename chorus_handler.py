3720e5511aff48ea252652b36f365fe6dd2a5304
Anthony Luo <anthonyluo100@gmail.com>
2019-12-03 17:44:49 -0500

    now reformated

--- chorus_handler.py
+++ chorus_handler.py
@@ -1,8 +1,9 @@
 from multiprocessing import Manager, Process
-from music_player import play_spongebob_theme,  play_super_mario
+from music_player import play_spongebob_theme, play_super_mario
 from spongebob_printer import print_all_spongebob
 from LED_code import Who_lives_in_a_pineapple, Yellow_and_Porous, SPONGE_BOB
 import time
+
 
 def play_original(has_moved):
     """
@@ -24,14 +25,17 @@ def play_new():
         time.sleep(4)
         play_spongebob_theme()
 
+
 def print_new():
     print_all_spongebob()
+
 
 def LED_new():
     time.sleep(4)
     SPONGE_BOB()
     Who_lives_in_a_pineapple()
     Yellow_and_Porous()
+
 
 def handler(mver=0):
     """
@@ -41,8 +45,8 @@ def handler(mver=0):
         has_moved = manager.Value('b', False)
         processOriginal = Process(target=play_original, args=(has_moved))
         processNew = Process(target=play_new, args=())
-        processNewPrinter = Process(target = print_new, args = ())
-        processLED = Process(target = LED_new(), args =())
+        processNewPrinter = Process(target=print_new, args=())
+        processLED = Process(target=LED_new(), args=())
     if mver == 0:
         processOriginal.start()
         processOriginal.join()
